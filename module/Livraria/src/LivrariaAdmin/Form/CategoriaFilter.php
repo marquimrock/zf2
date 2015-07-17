2a4f78a6d050da2e9a1c255fe9184c4c2cd75d8d
marquimrock <marquim.ti@gmail.com>
2015-07-17 17:05:29 -0300

    teste

--- module/Livraria/src/LivrariaAdmin/Form/CategoriaFilter.php
+++ module/Livraria/src/LivrariaAdmin/Form/CategoriaFilter.php
@@ -8,17 +8,17 @@ class CategoriaFilter extends InputFilter {
 
     public function __construct() {
         $this->add(array(
-           'name' => 'nome',
+            'name' => 'nome',
             'required' => true,
             'filters' => array(
-                array('name'=>'StripTags'),
-                array('name'=>'StringTrim')
+                array('name' => 'StripTags'),
+                array('name' => 'StringTrim')
             ),
             'validators' => array(
                 array(
                     'name' => 'NotEmpty',
-                    'options'=>array(
-                        'messages' => array('isEmpty'=>'Nome não pode estar em branco'),
+                    'options' => array(
+                        'messages' => array('isEmpty' => 'Nome não pode estar em branco'),
                     )
                 )
             )
